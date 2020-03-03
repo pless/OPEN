8243e09945599d5a42475b43e607e48b923efaff
ZongyangLi <zongyangli86@gmail.com>
2020-03-03 15:05:12 -0600

    Add files via upload

--- crop/error_data_utils.py
+++ crop/error_data_utils.py
@@ -4,7 +4,7 @@ Created on Feb 18, 2020
 @author: zli
 '''
 
-import sys, os, json
+import sys, os, json, shutil
 import cv2
 
 def load_json(meta_path):
@@ -77,7 +77,10 @@ def full_dir_scan(in_dir):
     
     return
 
-def bgr_2_rgb(in_dir):
+def bgr_2_rgb(in_dir, out_dir):
+    
+    if not os.path.isdir(out_dir):
+        os.makedirs(out_dir)
     
     plot_dirs = os.listdir(in_dir)
     for d in plot_dirs:
@@ -85,22 +88,35 @@ def bgr_2_rgb(in_dir):
         if not os.path.isdir(in_path):
             continue
         
+        out_path = os.path.join(out_dir, d)
+        if not os.path.isdir(out_path):
+            os.mkdir(out_path)
+        
         plot_full_path = in_path
         file_lists = os.walk(plot_full_path)
         for root, dirs, files in file_lists:
             for f in files:
                 if f.endswith('png'):
-                    file_path = os.path.join(plot_full_path, f)
-                    bgr_img = cv2.imread(file_path, cv2.IMREAD_COLOR)
+                    rgb_path = os.path.join(plot_full_path, f)
+                    bgr_img = cv2.imread(rgb_path, cv2.IMREAD_COLOR)
                     rgb_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2RGB)
-                    cv2.imwrite(file_path, rgb_img)
+                    
+                    out_rgb_path = os.path.join(out_path, f)
+                    cv2.imwrite(out_rgb_path, rgb_img)
+                    
+                    json_baseName = f[:-3]+'json'
+                    src_json_path = os.path.join(in_path, json_baseName)
+                    if not os.path.isfile(src_json_path):
+                        continue
+                    dst_json_path = os.path.join(out_path, json_baseName)
+                    shutil.copyfile(src_json_path, dst_json_path)
                 
     return
 
 
 def main():
     
-    bgr_2_rgb('/data/shared/stereo_s4_result_v2/')
+    bgr_2_rgb('/media/zli/Seagate Backup Plus Drive/OPEN/ua-mac/Level_2/stereoTop', '/media/zli/Seagate Backup Plus Drive/OPEN/ua-mac/Level_2/stereoTop_v2')
     #bgr_2_rgb('/media/zli/Seagate Backup Plus Drive/OPEN/ua-mac/Level_2/stereoTop')
 
 
