10bdfa5c348818a93fa746b8edb2f8b5c7cc6af3
Zhang Zeyu <andrew.zh@live.com>
2020-03-30 10:34:47 -0400

    remove folder naming number of digits constrain

--- crop/stereo.py
+++ crop/stereo.py
@@ -472,7 +472,7 @@ def singe_image_process(in_dir, out_dir, plot_dir, convt):
     
     # save image
     
-    save_dir = '{0:02d}-{1:02d}-{2:03d}'.format(plot_row, plot_col, plotNum)
+    save_dir = '{0}_{1}_{2}'.format(plot_row, plot_col, plotNum)
     s_d = os.path.join(plot_dir, save_dir)
     if not os.path.isdir(s_d):
         os.mkdir(s_d)
