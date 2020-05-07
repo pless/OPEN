c7066bb6ae92ca8d4263c16d848ed595cc1fe954
Zeyu Zhang <andrew.zh@live.com>
2020-05-07 00:51:26 -0400

    add thermal to init

--- crop/__init__.py
+++ crop/__init__.py
@@ -1,2 +1,3 @@
 from .scanner3d import scanner3d_single_crop
-from .stereo import full_day_multi_process as stereo_single_crop
+from .stereo import singe_image_process as stereo_single_crop
+from .thermal import singe_image_process as thermal_single_crop
